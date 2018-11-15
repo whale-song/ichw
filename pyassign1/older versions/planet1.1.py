11654a66d1d73c797dbec56c6c0e790061c24cc9
宋怀雨 <whale-song@users.noreply.github.com>
2018-11-15 20:50:44 +0800

    updates

--- pyassign1/older versions/planet1.1.py
+++ pyassign1/older versions/planet1.1.py
@@ -1,3 +1,10 @@
+# 作者：宋怀雨
+# 化学与分子工程学院
+
+# v1.1更新说明
+# 优化了行星运行速度和流畅性。
+
+
 import turtle
 import math as m
 
@@ -9,7 +16,7 @@ e = turtle.Turtle()
 f = turtle.Turtle()
 g = turtle.Turtle()
 
-#Sun
+# Sun
 a.speed(0)
 a.hideturtle()
 a.penup()
@@ -21,9 +28,9 @@ a.begin_fill()
 a.circle(25)
 a.end_fill()
 
-#Mercury
-#a=58000000, b=56700000, e=0.205630, c=11926540
-#T=87.9d
+# Mercury
+# a=58000000, b=56700000, e=0.205630, c=11926540
+# T=87.9d
 b.color(175/255, 175/255, 175/255)
 b.turtlesize(0.5)
 b.hideturtle()
@@ -39,9 +46,9 @@ def mercury(i):
     y=56.7 * m.sin(3.14*i/180)
     b.goto(x,y)
     
-#Venus
-#a=108208000, b=108206759, e=0.006772, c=732784
-#T=224.7d
+# Venus
+# a=108208000, b=108206759, e=0.006772, c=732784
+# T=224.7d
 c.color(238/255, 201/255, 0/255)
 c.turtlesize(0.8)
 c.hideturtle()
@@ -57,9 +64,9 @@ def venus(i):
     y=107 * m.sin(3.14*i/180)
     c.goto(x,y)
 
-#Earth
-#a=149598023, b=14957580, e=0.0167086, c=2499573
-#T=365d
+# Earth
+# a=149598023, b=14957580, e=0.0167086, c=2499573
+# T=365d
 d.color(32/255, 178/255, 170/255)
 d.turtlesize(1)
 d.hideturtle()
@@ -76,9 +83,9 @@ def earth(i):
     d.goto(x,y)
     
 
-#Mars
-#a=227939200, b=227440455, e=0.0934, c=212895214
-#T=693.5d
+# Mars
+# a=227939200, b=227440455, e=0.0934, c=212895214
+# T=693.5d
 e.color(165/255, 42/255, 42/255)
 e.turtlesize(1.2)
 e.hideturtle()
@@ -94,10 +101,10 @@ def mars(i):
     y=227 * m.sin(3.14*i/180)
     e.goto(x,y)
 
-#Jupiter
-#(该行星轨道距太阳过远，已按0.5倍比例缩放其轨道)
-#a=778570000, b=777638580, e=0.0489, c=38072073
-#T=4309d
+# Jupiter
+# (该行星轨道距太阳过远，已按0.5倍比例缩放其轨道)
+# a=778570000, b=777638580, e=0.0489, c=38072073
+# T=4309d
 f.color(255/255, 165/255, 0/255)
 f.turtlesize(3)
 f.hideturtle()
@@ -113,10 +120,10 @@ def jupiter(i):
     y= 388 * m.sin(3.14*i/180)
     f.goto(x,y)
 
-#Saturn
-#(该行星轨道距太阳过远，已按04倍例缩放其轨道)
-#a=1433530000, b=1431240078, e=0.0565, c=80994445
-#T=10767d
+# Saturn
+# (该行星轨道距太阳过远，已按0.4倍例缩放其轨道)
+# a=1433530000, b=1431240078, e=0.0565, c=80994445
+# T=10767d
 g.color(139/255, 69/255, 19/255)
 g.turtlesize(2.5)
 g.hideturtle()
@@ -135,24 +142,18 @@ def saturn(i):
 
 if __name__ == '__main__':
 	while True:
-		for n in range(360*6):
-			x1 = int(n/6)
+		for n in range(360*122):
 			if n%6 == 0:
-				for p in range(122*x1, 122*(1+x1)):
-					mercury(p)
+				mercury(n)
 			elif n%6 == 1:
-				for p in range(48*x1, 48*(x1+1)):
-					venus(p)
+				venus(48*n/122)
 			elif n%6 == 2:
-				for p in range(29*x1, 29*(x1+1)):
-					earth(p)
+				earth(29*n/122)
 			elif n%6 == 3:
-				for p in range(15*x1, 15*(x1+1)):
-					mars(p)
+				mars(15*n/122)
 			elif n%6 == 4:
-				for p in range(3*x1, 3*(x1+1)):
-					jupiter(p)
+				jupiter(3*n/122)
 			else:
-				saturn(x1)
+				saturn(n/122)
 
 
--- pyassign1/planet1.4.0.py
+++ pyassign1/planet1.4.0.py
@@ -1,16 +1,13 @@
 # 作者：宋怀雨
 # 化学与分子工程学院
 
-# v1.3.3更新说明
-# 增加了日期显示乌龟，同步协调运行日期显示。
-# 修约日期显示小数为1，调整了日期刷新频率。解决了日期龟设置颜色失败的bug。
-# 完成一周轨迹描画后各行星现在将不再描绘轨迹，提高了长时间运行情况下程序的运行速度和流畅性。
-# 修复了年份显示错误的bug。
+# v1.4.0更新说明
+# 加了一个月亮！OvO
 
 import turtle
 import math as m
 
-#注册turtle
+# 注册turtle
 a = turtle.Turtle()
 b = turtle.Turtle()
 c = turtle.Turtle()
@@ -18,24 +15,43 @@ d = turtle.Turtle()
 e = turtle.Turtle()
 f = turtle.Turtle()
 g = turtle.Turtle()
+h = turtle.Turtle()
 w = turtle.Turtle()
+morbit = turtle.Turtle()
 
-#总运行数据组
-#pa：半长轴
-#pb：半短轴
-#pc：1/2焦距
-planet = [b, c, d, e, f, g]
-pa = [58, 108, 150, 228, 389, 573]
+# 总运行数据组
+# pa：半长轴
+# pb：半短轴
+# pc：1/2焦距
+planet = [b, c, d, e, f, g, h]
+pa = [58, 108, 150, 228, 389, 573, 0]
 pb = [56.7, 107, 150, 227, 388, 572]
-pc = [11, 0.7, 2, 21, 19,32]
-size = [0.5, 0.8, 1, 1.2, 3, 2.5]
+pc = [11, 0.7, 2, 21, 19,32, 0]
+size = [0.5, 0.8, 1, 1.2, 3, 2.5, 0.2]
 
-#参数初始化
+# 参数初始化
+for i in range(7):
+	planet[i].hideturtle()
+	planet[i].turtlesize(size[i])
+	planet[i].shape('circle')
+	planet[i].speed(0)
+	planet[i].penup()
+	planet[i].fd( pa[i] + pc[i] )
+	planet[i].pendown()
+	planet[i].showturtle()
+#	planet[i].speed(9)
+
 turtle.bgcolor('black')
 w.hideturtle()
+h.penup()
 j = 0
 
-#Sun
+morbit.lt(90) #初始化月球轨道
+morbit.hideturtle() 
+morbit.pensize(0.3)
+morbit.speed(0)
+
+# Sun
 a.speed(0)
 a.hideturtle()
 a.penup()
@@ -47,25 +63,17 @@ a.begin_fill()
 a.circle(25)
 a.end_fill()
 
-#行星初始化
+# 行星初始化
 b.color(175/255, 175/255, 175/255)
 c.color(238/255, 201/255, 0/255)
 d.color(32/255, 178/255, 170/255)
 e.color(165/255, 42/255, 42/255)
 f.color(255/255, 165/255, 0/255)
 g.color(139/255, 69/255, 19/255)
+h.color('white')
 w.color('white')
+morbit.color('white')
 
-for i in range(6):
-	planet[i].hideturtle()
-	planet[i].turtlesize(size[i])
-	planet[i].shape('circle')
-	planet[i].speed(0)
-	planet[i].penup()
-	planet[i].fd( pa[i] + pc[i] )
-	planet[i].pendown()
-	planet[i].showturtle()
-	planet[i].speed(9)
 
 #planame = ['mercury', 'venus', 'earth', 'mars', 'jupiter', 'saturn']
 
@@ -75,9 +83,9 @@ for i in range(6):
 #		y = pb[p] * m.sin(3.14*i/180)
 #		planet[p].goto(x,y)
 
-#Mercury
-#a=58000000, b=56700000, e=0.205630, c=11926540
-#T=87.9d
+# Mercury
+# a=58000000, b=56700000, e=0.205630, c=11926540
+# T=87.9d
 def mercury(i):
     x=58 * m.cos(3.14*i/180)+11
     y=56.7 * m.sin(3.14*i/180)
@@ -85,9 +93,9 @@ def mercury(i):
     if i >= 360:
     	b.penup()
 
-#Venus
-#a=108208000, b=108206759, e=0.006772, c=732784
-#T=224.7d
+# Venus
+# a=108208000, b=108206759, e=0.006772, c=732784
+# T=224.7d
 def venus(i):
     x=108 * m.cos(3.14*i/180)+0.7
     y=107 * m.sin(3.14*i/180)
@@ -95,19 +103,30 @@ def venus(i):
     if i >= 360:
     	c.penup()
 
-#Earth
-#a=149598023, b=14957580, e=0.0167086, c=2499573
-#T=365d	
+# Earth
+# a=149598023, b=14957580, e=0.0167086, c=2499573
+# T=365d	
 def earth(i):
     x=150 * m.cos(3.14*i/180)+2
     y=150 * m.sin(3.14*i/180)
+    morbit.goto(x+15,y)	
+    morbit.clear()
     d.goto(x,y)
+    morbit.circle(15)
     if i >= 360:
     	d.penup()
 
-#mars
-#a=227939200, b=227440455, e=0.0934, c=212895214
-#T=693.5d
+# Moon
+# a=384399, e=0.05(此处视为圆轨道)
+# T=30d
+def moon(i):
+	x2=(150 * m.cos(3.14*i/180)+2)+15*m.cos(12*(3.14*i/180))
+	y2=150 * m.sin(3.14*i/180)+15*m.sin(12*(3.14*i/180))
+	h.goto(x2,y2)
+	
+# Mars
+# a=227939200, b=227440455, e=0.0934, c=212895214
+# T=693.5d
 def mars(i):
     x=228 * m.cos(3.14*i/180)+21
     y=227 * m.sin(3.14*i/180)
@@ -115,10 +134,10 @@ def mars(i):
     if i >= 360:
     	e.penup()
 
-#Jupiter
-#(该行星轨道距太阳过远，已按0.5倍比例缩放其轨道)
-#a=778570000, b=777638580, e=0.0489, c=38072073
-#T=4309d
+# Jupiter
+# (该行星轨道距太阳过远，已按0.5倍比例缩放其轨道)
+# a=778570000, b=777638580, e=0.0489, c=38072073
+# T=4309d
 def jupiter(i):
     x= 389 * m.cos(3.14*i/180)+19
     y= 388 * m.sin(3.14*i/180)
@@ -126,10 +145,10 @@ def jupiter(i):
     if i >= 360:
     	f.penup()
 
-#Saturn
-#(该行星轨道距太阳过远，已按0.4倍例缩放其轨道)
-#a=1433530000, b=1431240078, e=0.0565, c=80994445
-#T=10767d
+# Saturn
+# (该行星轨道距太阳过远，已按0.4倍例缩放其轨道)
+# a=1433530000, b=1431240078, e=0.0565, c=80994445
+# T=10767d
 def saturn(i):
     x=573 * m.cos(3.14*i/180)+32
     y=572 * m.sin(3.14*i/180)
@@ -137,7 +156,7 @@ def saturn(i):
     if i >= 360:
     	g.penup()
 
-#Date
+# Date
 # 1 degree = (87.9/360)d
 def date(i):
 	x = -(int(m.log10(int(i)+1)) * 9 + 20)
@@ -147,7 +166,10 @@ def date(i):
 	w.penup()
 	w.goto(x, y)
 	w.pendown()
-	w.write(str('%.1f' % i) + "year", font=("Arial", 12, "bold"))
+	if ('%.1f' % i)==11.3:
+		w.write('iG牛逼！')
+	else:
+		w.write(str('%.1f' % i) + "year", font=("Arial", 12, "bold"))
 
 
 if __name__ == '__main__':
@@ -162,7 +184,8 @@ if __name__ == '__main__':
 				venus(48*n/122)
 			elif n%6 == 2:
 				earth(29*n/122)
-				j += (168.5/43920)
+				moon(29*n/122)
+				j += (168.6/43920)
 			elif n%6 == 3:
 				mars(15*n/122)
 			elif n%6 == 4:
